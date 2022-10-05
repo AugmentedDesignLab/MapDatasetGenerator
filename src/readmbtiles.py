74c1dfea1a4f1cc07bf69b6c7c830605ed084d5b
ishaan95 <ishaan.paranjape@gmail.com>
2022-10-05 14:03:16 -0700

    created a src folder for packaging

---
+++ src/readmbtiles.py
@@ -0,0 +1,3 @@
+import mbutil
+
+mbutil.mbtiles_to_disk('../data/input/mbtiles/PaloAlto-mbtiles-openmaptiles/PaloAlto.mbtiles', 'data/output/tiles')
--- src/sliceimages.py
+++ src/sliceimages.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 from matplotlib import image as mpimg
 
-np_image = mpimg.imread('data/output/SanJose3.png')
+np_image = mpimg.imread('../data/output/SanJose3.png')
 i=0
 
 
