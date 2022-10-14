cde88dfef082188df60a8b5bff783badea622d1b
adhocmaster <adhocmaster@live.com>
2022-10-14 16:23:27 -0700

    succesful packaging

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Generate and load dataset of road network maps.
 * Use pip to install necessary python packages `pip install -r requirements.txt`.
 * Run `python run.py` script. It will store .dill files in the data/output/ directory. 
 * These dill files can be read using the `ImgGroupReader` object in `read.py`. Run `read.py` to run a small test for the same.  
+
+
+# Installation from pip
+
+```
+pip install mapdatasetgenerator
+```
+
+# Usage
+```
+import mapdataset
+```
---
+++ test.py
@@ -0,0 +1,24 @@
+# Script to read dill data objects as numpy arrays.
+import dill
+import numpy as np
+from PIL import Image
+import os
+
+from mapdataset import ImageGroupReader
+
+
+dillFolder = "./data/output/SF_Layered/32x32/stride-10"
+nGroups = 0
+# Iterate directory
+for path in os.listdir(dillFolder):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dillFolder, path)) and path.endswith(".dill"):
+        nGroups += 1
+
+for i in range(nGroups):
+    reader = ImageGroupReader(dillFolder)
+    data = reader.load_group(groupNo=i)
+    patchImgArray = reader.asImg(data[0])
+    im = Image.fromarray(patchImgArray)
+    path = os.path.join(dillFolder, f"{i}-0.png")
+    im.save(path)
